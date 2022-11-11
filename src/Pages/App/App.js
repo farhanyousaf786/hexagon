dce1f4aa56f68ce6975a07cae8182766804f2edd
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-11 15:22:59 -0500

    setting defualt pages

---
+++ src/Pages/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {Route, Routes} from "react-router-dom";
+import LandingPage from '../LandingPage/LandingPage';
+
+
+function App() {
+
+  return (
+    <div className="app-background">
+    <Routes>
+    <Route  path="/" element={<LandingPage />} />
+    </Routes>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+function LandingPage() {
+
+    return (
+    
+      <h3>Landing page is here..</h3>
+     
+    )
+  
+}
+
+export default LandingPage;
