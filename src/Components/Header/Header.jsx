21ce8a415b9be0ddffd5f67ee7fe67d92d5a936d
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-11 17:29:52 -0500

    added pages

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import './Header.css';
 
 
-const Header = () => {
+function Header ({homeClick, productsClick, servicesClick, galleryClick, aboutClick}){
   // Sticky Menu Area
   useEffect(() => {
     window.addEventListener("scroll", isSticky);
@@ -19,28 +19,26 @@ const Header = () => {
       ? header.classList.add("is-sticky")
       : header.classList.remove("is-sticky");
   };
+
+
+
+
   return (
     <>
-      <header className="header-section d-none d-xl-block">
-
-
-      <header class="header">
-  <a href="" class="logo">CSS Nav</a>
-  <input class="menu-btn" type="checkbox" id="menu-btn" />
-  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-  <ul class="menu">
-    <li><a href="#work">Home</a></li>
-    <li><a href="#about">Products</a></li>
-    <li><a href="#careers">Services</a></li>
-    <li><a href="#contact">Gallery</a></li>
-    <li><a href="#contact">About Us</a></li>
-
-  </ul>
-</header>
-
-
-
-      </header>
+    <header className="header-section d-none d-xl-block">
+    <div class="header">
+    <a href="" class="logo">CSS Nav</a>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+    <li><a onClick={homeClick} href="#home">Home</a></li>
+    <li><a onClick={productsClick} href="#Products">Products</a></li>
+    <li><a onClick={servicesClick} href="#Services">Services</a></li>
+    <li><a onClick={galleryClick} href="#Gallery">Gallery</a></li>
+    <li><a onClick={aboutClick} href="#About">About Us</a></li>
+    </ul>
+    </div>
+    </header>
     </>
   );
 };
