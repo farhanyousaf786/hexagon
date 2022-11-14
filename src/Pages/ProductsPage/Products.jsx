9d9fdbdca9228c1f0c04b47608ad3c574f2a080d
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-14 13:55:18 -0500

    added resposive font size to top logo

--- src/Pages/ProductsPage/Products.jsx
+++ src/Pages/ProductsPage/Products.jsx
@@ -7,7 +7,27 @@ const Products = () => {
   return (
     
     <div className="pro-container">
-      <h1>Product Page is here</h1>
+      <div className="intro">
+      <h1>HEXAGON SCAFFOLDING INC.</h1>
+      <h3>SUSPENDED SCAFFOLD RENTALS, SALES & SERVICES</h3>
+      <h4>OUR MISSION</h4>
+      </div>
+
+      <div>
+     <h1>OUR PRODUCTS</h1>
+
+     <div id="slider-1">
+     <figure>
+     <img src="https://imgur.com/DaT4L0R.png" alt/>
+     <img src="https://imgur.com/DaT4L0R.png" alt/>
+     <img src="https://imgur.com/DaT4L0R.png" alt/>
+     <img src="https://imgur.com/DaT4L0R.jpg" alt/>
+     <img src="https://imgur.com/DaT4L0R.jpg" alt/>
+     </figure>
+     </div>
+     
+      </div>
+      
     </div>
   );
 };
