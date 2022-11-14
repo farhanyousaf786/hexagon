38712c621927393bc84a1e8342495ae6428d98b5
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-14 10:39:25 -0500

    added imgs

--- src/Pages/FrontPage/FrontPage.jsx
+++ src/Pages/FrontPage/FrontPage.jsx
@@ -6,19 +6,22 @@ const FrontPage = () => {
  
   return (
     
-    <div className="fp-container">
-   
+     <div className="fp-container">
      <div id="slider">
-<figure>
-<img src="https://imgur.com/DaT4L0R.png" alt/>
-<img src="https://imgur.com/DaT4L0R.png" alt/>
-<img src="https://imgur.com/DaT4L0R.png" alt/>
-<img src="https://imgur.com/DaT4L0R.jpg" alt/>
-<img src="https://imgur.com/DaT4L0R.jpg" alt/>
-</figure>
-</div>
-
-    </div>
+     <figure>
+     <img src="https://imgur.com/DaT4L0R.png" alt/>
+     <img src="https://imgur.com/DaT4L0R.png" alt/>
+     <img src="https://imgur.com/DaT4L0R.png" alt/>
+     <img src="https://imgur.com/DaT4L0R.jpg" alt/>
+     <img src="https://imgur.com/DaT4L0R.jpg" alt/>
+     </figure>
+     </div>
+     <div className="shade">
+     </div>
+     <div class="logo">
+     <h2>child 2</h2>
+     </div>
+     </div> 
   );
 };
 
