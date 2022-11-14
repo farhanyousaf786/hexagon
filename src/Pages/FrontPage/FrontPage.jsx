f6f287493bc0e3ee9b00945e7928bc56236a9560
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-14 15:03:10 -0500

    edited css

--- src/Pages/FrontPage/FrontPage.jsx
+++ src/Pages/FrontPage/FrontPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './FrontPage.css';
+import './FrontPage.scss';
 
 
 const FrontPage = () => {
@@ -7,14 +7,59 @@ const FrontPage = () => {
   return (
     
      <div className="fp-container">
-     <div id="slider">
-     <figure>
-     <img src="https://imgur.com/DaT4L0R.png" alt/>
-     <img src="https://imgur.com/DaT4L0R.png" alt/>
-     <img src="https://imgur.com/DaT4L0R.png" alt/>
-     <img src="https://imgur.com/DaT4L0R.jpg" alt/>
-     <img src="https://imgur.com/DaT4L0R.jpg" alt/>
-     </figure>
+
+
+
+
+     <div class="slider">
+	<div class="slide-track">
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		</div>
+	
+</div>
+
+
+
      </div>
      <div className="shade">
      </div>
