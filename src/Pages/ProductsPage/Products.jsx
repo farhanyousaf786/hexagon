7be1bdfab4a0b17884e7ac18b35ec1626659983e
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-14 14:37:54 -0500

    added resposive font size to top logo

--- src/Pages/ProductsPage/Products.jsx
+++ src/Pages/ProductsPage/Products.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Products.css';
+import './Products.scss';
 
 
 const Products = () => {
@@ -16,16 +16,57 @@ const Products = () => {
       <div>
      <h1>OUR PRODUCTS</h1>
 
-     <div id="slider-1">
-     <figure>
-     <img src="https://imgur.com/DaT4L0R.png" alt/>
-     <img src="https://imgur.com/DaT4L0R.png" alt/>
-     <img src="https://imgur.com/DaT4L0R.png" alt/>
-     <img src="https://imgur.com/DaT4L0R.jpg" alt/>
-     <img src="https://imgur.com/DaT4L0R.jpg" alt/>
-     </figure>
-     </div>
-     
+       <div className="slide-box">
+
+       <div class="slider">
+	<div class="slide-track">
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		</div>
+		<div class="slide">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		</div>
+	</div>
+</div>
+
+
+       </div>
       </div>
       
     </div>
