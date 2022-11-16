65f7eeb0a55cdade7a63383d5f898906179f856b
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-16 16:08:03 -0500

    edited css

--- src/Components/GoogleMap/GoogleMap.jsx
+++ src/Components/GoogleMap/GoogleMap.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
+import LocationPin from "../LocationPin/LocationPin";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-export default function SimpleMap(){
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
+export default function SimpleMap({ location, zoomLevel }){
+ 
+  
 
   return (
-    // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
+    
+
+    <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+    <div className="google-map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
       >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
         />
       </GoogleMapReact>
+    </div>
     </div>
   );
 }
--- src/Pages/AboutPage/About.jsx
+++ src/Pages/AboutPage/About.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import SimpleMap from "../../Components/GoogleMap/GoogleMap";
 import './About.css';
 
 
 const About = () => {
+  const location = {
+    address: '1600 Amphitheatre Parkway, Mountain View, california.',
+    lat: 40.716280,
+    lng: -74.199530
+  }
  
   return (
     
@@ -12,7 +18,7 @@ const About = () => {
       <a>369 Jellif Ave | Newark | NJ 07108</a><br/>
       <a>PHONE: (718) 725 8969 EXT 05</a>
       </p>
-      
+      < SimpleMap location={location}  zoomLevel={9}/>
       
     
     </div>
