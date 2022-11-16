169663b152ef8267f360d61d8a58c9e96d3d9134
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-16 15:46:29 -0500

    edited css

--- src/Pages/AboutPage/About.jsx
+++ src/Pages/AboutPage/About.jsx
@@ -7,6 +7,9 @@ const About = () => {
   return (
     
     <div className="about-container">
+      <h1>HEXAGON SCAFFOLDING INC</h1>
+      <h1>369 jellif Ave | Newark | NJ 07108</h1>
+      <h1>About Page is here</h1>
       <h1>About Page is here</h1>
     </div>
   );
--- src/Pages/ServicesPage/Services.jsx
+++ src/Pages/ServicesPage/Services.jsx
@@ -6,9 +6,11 @@ const Services = () => {
  
   return (
     
-    <div className="ser-container">
-      <h1>Services Page is here</h1>
-    </div>
+     <div className="ser-container">
+     <center><h1>SERVICES</h1></center>
+     <div className="s-logo">
+     <img  className="img" src="https://imgur.com/m2CGOHJ.jpg" alt/>
+     </div>    </div>
   );
 };
 
