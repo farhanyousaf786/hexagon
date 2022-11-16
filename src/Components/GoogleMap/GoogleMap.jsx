95cc22ab6de751082b86a77e1b807d4d5fbd1280
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-16 16:26:18 -0500

    added google map

--- src/Components/GoogleMap/GoogleMap.jsx
+++ src/Components/GoogleMap/GoogleMap.jsx
@@ -1,31 +1,17 @@
 import React from "react";
-import GoogleMapReact from 'google-map-react';
-import LocationPin from "../LocationPin/LocationPin";
 
 
-export default function SimpleMap({ location, zoomLevel }){
- 
+export default function SimpleMap(){
   
 
   return (
+    <>
     
-
-    <div className="map">
-    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
-
-    <div className="google-map">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: '' }}
-        defaultCenter={location}
-        defaultZoom={zoomLevel}
-      >
-        <LocationPin
-          lat={location.lat}
-          lng={location.lng}
-          text={location.address}
-        />
-      </GoogleMapReact>
-    </div>
-    </div>
+    <h1>
+     
+     <a href="https://www.google.com/maps/place/369+Jelliff+Ave,+Newark,+NJ+07108/@40.6915648,-74.1664621,12.12z/data=!4m18!1m12!4m11!1m3!2m2!1d-74.1991543!2d40.7178627!1m6!1m2!1s0x89c253234e0433cb:0x663d7ff15db9c6dc!2s369+Jelliff+Ave+Newark,+NJ+07108!2m2!1d-74.1994976!2d40.7161713!3m4!1s0x89c253234e0433cb:0x663d7ff15db9c6dc!8m2!3d40.7161713!4d-74.1994976">click</a>
+    </h1>
+    </>
+   
   );
 }
