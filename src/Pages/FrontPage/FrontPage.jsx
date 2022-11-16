a754230e0088e05445ed3703faad1249d300c855
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-16 12:23:57 -0500

    edited css

--- src/Pages/FrontPage/FrontPage.jsx
+++ src/Pages/FrontPage/FrontPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './FrontPage.scss';
+import './FrontPage.css';
 
 
 const FrontPage = () => {
@@ -8,20 +8,74 @@ const FrontPage = () => {
     
      <div className="fp-container">
 
-<div class="carousel-wrapper">
-  <div class="carousel-container">
-    <div class="carousel">
-      <div class="image-one"></div>
-      <div class="image-two"></div>
-      <div class="image-three"></div>
-    </div>
-  </div>
-</div>
 
-     <div className="logo">
+<section class="carousel" aria-label="Gallery">
+  <ol class="carousel__viewport">
+    <li id="carousel__slide1"
+        tabindex="0"
+        class="carousel__slide">
+      <div class="carousel__snapper">
+        <a href="#carousel__slide4"
+           class="carousel__prev">Go to last slide</a>
+        <a href="#carousel__slide2"
+           class="carousel__next">Go to next slide</a>
+      </div>
+    </li>
+    <li id="carousel__slide2"
+        tabindex="0"
+        class="carousel__slide">
+      <div class="carousel__snapper"></div>
+      <a href="#carousel__slide1"
+         class="carousel__prev">Go to previous slide</a>
+      <a href="#carousel__slide3"
+         class="carousel__next">Go to next slide</a>
+    </li>
+    <li id="carousel__slide3"
+        tabindex="0"
+        class="carousel__slide">
+      <div class="carousel__snapper"></div>
+      <a href="#carousel__slide2"
+         class="carousel__prev">Go to previous slide</a>
+      <a href="#carousel__slide4"
+         class="carousel__next">Go to next slide</a>
+    </li>
+    <li id="carousel__slide4"
+        tabindex="0"
+        class="carousel__slide">
+      <div class="carousel__snapper"></div>
+      <a href="#carousel__slide3"
+         class="carousel__prev">Go to previous slide</a>
+      <a href="#carousel__slide1"
+         class="carousel__next">Go to first slide</a>
+    </li>
+  </ol>
+  <aside class="carousel__navigation">
+    <ol class="carousel__navigation-list">
+      <li class="carousel__navigation-item">
+        <a href="#carousel__slide1"
+           class="carousel__navigation-button">Go to slide 1</a>
+      </li>
+      <li class="carousel__navigation-item">
+        <a href="#carousel__slide2"
+           class="carousel__navigation-button">Go to slide 2</a>
+      </li>
+      <li class="carousel__navigation-item">
+        <a href="#carousel__slide3"
+           class="carousel__navigation-button">Go to slide 3</a>
+      </li>
+      <li class="carousel__navigation-item">
+        <a href="#carousel__slide4"
+           class="carousel__navigation-button">Go to slide 4</a>
+      </li>
+    </ol>
+  </aside>
+</section>
+
+<div className="logo">
      <img  className="img" src="https://imgur.com/m2CGOHJ.jpg" alt/>
      </div>
-     </div> 
+ </div>
+     
   );
 };
 
