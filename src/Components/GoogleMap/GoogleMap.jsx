2c34582324634462a0cf5ad86fda7bc4853f379f
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-16 16:33:49 -0500

    added google map

--- src/Components/GoogleMap/GoogleMap.jsx
+++ src/Components/GoogleMap/GoogleMap.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './GoogleMap.css';
 
 
 export default function SimpleMap(){
@@ -7,11 +8,18 @@ export default function SimpleMap(){
   return (
     <>
     
-    <h1>
-     
-     <a href="https://www.google.com/maps/place/369+Jelliff+Ave,+Newark,+NJ+07108/@40.6915648,-74.1664621,12.12z/data=!4m18!1m12!4m11!1m3!2m2!1d-74.1991543!2d40.7178627!1m6!1m2!1s0x89c253234e0433cb:0x663d7ff15db9c6dc!2s369+Jelliff+Ave+Newark,+NJ+07108!2m2!1d-74.1994976!2d40.7161713!3m4!1s0x89c253234e0433cb:0x663d7ff15db9c6dc!8m2!3d40.7161713!4d-74.1994976">click</a>
-    </h1>
+
+    <div className="map-img">
+    
+
+     <a href="https://www.google.com/maps/place/369+Jelliff+Ave,+Newark,+NJ+07108/@40.6915648,-74.1664621,12.12z/data=!4m18!1m12!4m11!1m3!2m2!1d-74.1991543!2d40.7178627!1m6!1m2!1s0x89c253234e0433cb:0x663d7ff15db9c6dc!2s369+Jelliff+Ave+Newark,+NJ+07108!2m2!1d-74.1994976!2d40.7161713!3m4!1s0x89c253234e0433cb:0x663d7ff15db9c6dc!8m2!3d40.7161713!4d-74.1994976">
+        <center><img src="https://imgur.com/QqMbSzz.png"  alt="" /></center>
+        </a>
+    </div>
     </>
    
   );
 }
+
+
+
