e8b02f2a26b1b478ef3a251aa4492fb6b59a5c2d
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-17 14:44:49 -0500

    added responsive ui

--- src/Pages/GalleryPage/GalleryPage.jsx
+++ src/Pages/GalleryPage/GalleryPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Gallery from "../../Components/Gallery/Gallery";
+import "./GalleryPage.css"
 
 
 const GalleryPage = () => {
@@ -7,6 +8,7 @@ const GalleryPage = () => {
   return (
     
     <div className="gal-container">
+      <h1>Gallery</h1>
       <Gallery/>
     </div>
   );
