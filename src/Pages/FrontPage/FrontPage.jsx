db09173d566d5e8bd1730a656b5029489b5bcc98
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-17 10:23:26 -0500

    upload img logo

--- src/Pages/FrontPage/FrontPage.jsx
+++ src/Pages/FrontPage/FrontPage.jsx
@@ -11,14 +11,15 @@ const FrontPage = () => {
      <div className="fp-container">
 
 <AliceCarousel autoPlay autoPlayInterval="3000" >
+
+      <img src="https://imgur.com/hXhZRyq.jpg" className="sliderimg"/>
       <img src="https://imgur.com/Hyppul2.jpg" className="sliderimg"/>
-      <img src="https://imgur.com/m2CGOHJ.jpg" className="sliderimg"/>
-      <img src="https://imgur.com/m2CGOHJ.jpg" className="sliderimg"/>
-      <img src="https://imgur.com/m2CGOHJ.jpg" className="sliderimg"/>
+      <img src="https://imgur.com/lDsOMwB.jpg" className="sliderimg"/>
+      <img src="https://imgur.com/HNkNRXs.jpg" className="sliderimg"/>
 </AliceCarousel>
 
 <div className="logo">
-     <img  className="img" src="https://imgur.com/m2CGOHJ.jpg" alt/>
+     <img  className="img" src="https://imgur.com/KWrxMRD.jpg" alt/>
      </div>
  </div>
      
