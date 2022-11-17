f07b0d7601e6458cd19acb37ae7f34d8e6b5b651
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-17 18:17:54 -0500

    app routed

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -10,15 +10,8 @@ const ProductList = () => {
   
   <div className="gallery-size">
 
-  <h1>Product List is Development..</h1>
-  <AliceCarousel autoPlay autoPlayInterval="3000" disableButtonsControls="true" >
-
-  <img src="https://imgur.com/lOFcrTD.jpg" className="sliderimg"/>
-  <img src="https://imgur.com/5tyzKN6.jpg" className="sliderimg"/>
-  <img src="https://imgur.com/GkbkoFG.jpg" className="sliderimg"/>
-  <img src="https://imgur.com/6bZiKHu.jpg" className="sliderimg"/>
-
-  </AliceCarousel></div>
+  <center><h2>Product is in  Development..</h2></center>
+  </div>
 
      
   );
