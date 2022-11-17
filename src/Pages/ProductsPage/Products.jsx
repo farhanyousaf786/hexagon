efe240e6d6ce84060e09eee5747b34347c0ec054
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-17 11:57:37 -0500

    addednproducts

--- src/Pages/ProductsPage/Products.jsx
+++ src/Pages/ProductsPage/Products.jsx
@@ -19,22 +19,22 @@ const Products = () => {
        <div class="slider">
 	<div class="slide-track">
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+			<img src="https://imgur.com/UEUC3gE.png" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+			<img src="https://imgur.com/X0Bj4Ki.png" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+			<img src="https://imgur.com/HTA0oEr.png" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+			<img src="https://imgur.com/Hz28o9A.png" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+			<img src="https://imgur.com/apHppsm.png" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+			<img src="https://imgur.com/ljIVRr5.png" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
 			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
