ae9ff672861ff7eab7f29707987b1bd15ecc3134
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-17 13:23:33 -0500

    added responsive ui

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -7,18 +7,22 @@ import "react-alice-carousel/lib/alice-carousel.css";
 const Gallery = () => {
  
   return (
-    
+  
+  <div className="gallery-size">
+  
+  <AliceCarousel autoPlay autoPlayInterval="3000" >
 
+  <img src="https://imgur.com/hXhZRyq.jpg" className="sliderimg"/>
+  <img src="https://imgur.com/Hyppul2.jpg" className="sliderimg"/>
+  <img src="https://imgur.com/lDsOMwB.jpg" className="sliderimg"/>
+  <img src="https://imgur.com/HNkNRXs.jpg" className="sliderimg"/>
 
-<AliceCarousel autoPlay autoPlayInterval="3000" >
-      <img src="https://imgur.com/hXhZRyq.jpg" className="sliderimg"/>
-      <img src="https://imgur.com/Hyppul2.jpg" className="sliderimg"/>
-      <img src="https://imgur.com/lDsOMwB.jpg" className="sliderimg"/>
-      <img src="https://imgur.com/HNkNRXs.jpg" className="sliderimg"/>
+  </AliceCarousel></div>
 
-</AliceCarousel>
      
   );
 };
+
+
 
 export default Gallery;
