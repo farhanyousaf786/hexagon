0383f352a31de05eb607fb6957e090bfc4ba36eb
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-17 16:10:28 -0500

    added responsive ui

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -12,10 +12,10 @@ const Gallery = () => {
   
   <AliceCarousel autoPlay autoPlayInterval="3000" disableButtonsControls="true" >
 
-  <img src="https://imgur.com/hXhZRyq.jpg" className="sliderimg"/>
-  <img src="https://imgur.com/Hyppul2.jpg" className="sliderimg"/>
-  <img src="https://imgur.com/lDsOMwB.jpg" className="sliderimg"/>
-  <img src="https://imgur.com/HNkNRXs.jpg" className="sliderimg"/>
+  <img src="https://imgur.com/lOFcrTD.jpg" className="sliderimg"/>
+  <img src="https://imgur.com/5tyzKN6.jpg" className="sliderimg"/>
+  <img src="https://imgur.com/GkbkoFG.jpg" className="sliderimg"/>
+  <img src="https://imgur.com/6bZiKHu.jpg" className="sliderimg"/>
 
   </AliceCarousel></div>
 
