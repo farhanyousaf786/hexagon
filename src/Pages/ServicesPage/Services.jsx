848b0710ba7c1823014207d23f743edfeb4e2ddd
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-17 10:10:54 -0500

    edit css

--- src/Pages/ServicesPage/Services.jsx
+++ src/Pages/ServicesPage/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
   return (
     
      <div className="ser-container">
-     <center><h1>SERVICES</h1></center>
+     <center><h1>OUR SERVICES</h1></center>
      <div className="s-logo">
      <img  className="img" src="https://imgur.com/xXfDawS.jpg" alt=""/>
      </div>
