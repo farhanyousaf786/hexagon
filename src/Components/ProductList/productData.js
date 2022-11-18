89f7a681f4ef929260f5586073dd50ae2daf00ea
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-18 12:10:41 -0500

    added list pages

--- src/Components/ProductList/productData.js
+++ src/Components/ProductList/productData.js
@@ -4,14 +4,14 @@ export default [
         "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/24.pdf"
     },
     {
-        "name": "WINSAFE SUPERMOD PLATFORMS MANUAL",
-        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/24.pdf"
+        "name": "WINSAFE SUPERMOD PLATFORM OPERATING INSTRUCTIONS",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/25.pdf"
     }, {
-        "name": "WINSAFE SUPERMOD PLATFORMS MANUAL",
-        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/24.pdf"
+        "name": "TRACTEL® THRUSAFE® MODULAR PLATFORM FOR CONFINED SPACES",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/26.pdf"
     }, {
-        "name": "WINSAFE SUPERMOD PLATFORMS MANUAL",
-        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/24.pdf"
+        "name": "TRACTEL® SOLSIT® BOSUN CHAIR MANUAL",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/27.pdf"
     },
     
 ]
