30f349f7dab02f41f3c5b0c81155e59a7d25ba8d
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-18 11:22:20 -0500

    edited details

---
+++ src/Components/ProductList/list1.js
@@ -0,0 +1,7 @@
+export default [
+    {
+        "name": "ReactJS",
+        "icon": "https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png"
+    },
+    
+]
