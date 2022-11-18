4598eee717886eeccc535c284613985164b36f9c
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-18 11:33:01 -0500

    farhna txt

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import './ProductList.css';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
+import cardsArr from './list1';
+
 
 
 const ProductList = () => {
- 
+
+  useEffect(() => {
+    
+console.log("tesing")
+
+  });
+
   return (
   
   <div className="gallery-size">
