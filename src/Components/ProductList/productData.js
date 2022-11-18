3b820272bd5af5399934a981094b05c9f13586a1
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-18 12:17:51 -0500

    added list pages

--- src/Components/ProductList/productData.js
+++ src/Components/ProductList/productData.js
@@ -1,4 +1,6 @@
 export default [
+
+    //-------------------------SUSPENDED SCAFFOLDING PLATFORMS-------------------------------//
     {
         "name": "WINSAFE SUPERMOD PLATFORMS MANUAL",
         "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/24.pdf"
@@ -6,12 +8,97 @@ export default [
     {
         "name": "WINSAFE SUPERMOD PLATFORM OPERATING INSTRUCTIONS",
         "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/25.pdf"
-    }, {
+    },
+     {
         "name": "TRACTEL® THRUSAFE® MODULAR PLATFORM FOR CONFINED SPACES",
         "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/26.pdf"
-    }, {
+    }, 
+    {
         "name": "TRACTEL® SOLSIT® BOSUN CHAIR MANUAL",
         "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/27.pdf"
     },
+    {
+        "name": "TRACTEL® SKYSAFE® MODULAR AND KNOCKDOWN PLATFORMS MANUAL",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/28.pdf"
+    },
+    {
+        "name": "TRACTEL® MODULAR SCAFFOLD VARIANCE",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/29.pdf"
+    },
+    {
+        "name": "TRACTEL® SOLSIT® BOSUN CHAIR MANUAL",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/30.pdf"
+    },
+    {
+        "name": "SKYCLIMBER MKD MODULAR PLATFORM MANUAL",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/31.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/32.pdf"
+    },
     
+
+    //---------------------------SUSPENDED SCAFFOLDING MOTORS-------------------------------------//
+
+    {
+        "name": "SPIDER SC1000 / SC1500 OPERATOR'S MANUAL",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/33.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/34.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/35.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/36.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/37.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/38.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/40.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/41.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/42.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/43.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/44.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/45.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/46.pdf"
+    },
+    {
+        "name": "SKY CLIMBER SERVICE BULLETIN #151",
+        "link": "https://switchappimages.nyc3.digitaloceanspaces.com/Hexagon/47.pdf"
+    },
+
+
+
+
 ]
